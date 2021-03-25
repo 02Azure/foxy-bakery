178c07367b6578304e09f87787994db781048055
02Azure <agusdomas1st@gmail.com>
2021-03-25 20:30:02 +0700

    bcryptjs implemented

--- seeders/20210325091759-user-seed.js
+++ seeders/20210325091759-user-seed.js
@@ -5,7 +5,7 @@ module.exports = {
     let users = [
       {
         username: "otong_322",
-        password: "pass123",
+        password: "$2a$10$owuk4k.Rw6sUUhyDOZdZAOrXYsoMyDLZw3Yercdin7xyDENfrv0ZC", //pass123
         email: "otong@mail.com",
         address: "Zuzu city no. 32, Albatros Country",
         phone: "07448342",
@@ -14,7 +14,7 @@ module.exports = {
       },
       {
         username: "02azure",
-        password: "secret",
+        password: "$2a$10$kGRKsK0R8FMsq543pk8OieVbeyx4zSI0OEfvbo2WfzVaYPY2GG.82", //secret
         email: "azure@gmail.com",
         address: "Pelican Town no. 17, Stardew Valley",
         phone: "07343213",
@@ -23,7 +23,7 @@ module.exports = {
       },
       {
         username: "lily_nano",
-        password: "lilily",
+        password: "$2a$10$8PeKL.mm3FpKaj22JIJvoeNJpap0FRTIFQJDuMp/n8ej3Yy8p1PQG", //lilily
         email: "lily_lily@mail.com",
         address: "Evercold city no. 3, Northren City",
         phone: "0834132",
